e9657580671bfcd3d4308c43e221d1044bf53a06
Natalie Cabral <nataliegc23@gmail.com>
2022-12-14 19:17:30 -0300

    updates

---
+++ app/addPlayer.js
@@ -0,0 +1,40 @@
+const placar = document.querySelector('.placar')
+const adicionar = document.querySelector('#btn-add')
+const tabela = document.querySelector('#tabela-jogadores')
+
+adicionar.addEventListener('click', () => {
+    const nomeJogador = document.querySelector('#iname').value;
+    jogadores.push({
+        nome: nomeJogador,
+        vitorias : 0,
+        empates: 0,
+        derrotas: 0,
+        pontos: 0
+    });
+
+    nomeJogador.value = "";    
+    
+    if (nomeJogador.length != 0) {
+        exibirJogadores(jogadores)
+    } else {
+        alert('[ERRO] O nome inserido é inválido.');
+    }
+});
+
+let jogadores = [maquina];
+
+
+function exibirJogadores(listaJogadores) {
+    tabela.innerHTML = '';
+    listaJogadores.forEach(jogador => {
+        tabela.innerHTML += `
+        <tr>
+            <td>${jogador.nome}</td>
+            <td>${jogador.vitorias}</td>
+            <td>${jogador.empates}</td>
+            <td>${jogador.derrotas}</td>
+            <td>${jogador.pontos}</td>
+        </tr>
+        `
+    })
+}
---
+++ app/main.js
@@ -0,0 +1,59 @@
+// finn, jake, princesa jujuba, princesa caroço, marceline, rei gelado, bmo, princesa de fogo, prismo, dona tromba, canelinha
+
+let personagens = []
+const personagensAPI = 'https://nataliegcabral.github.io/personagens/lista.json'
+const sortear = document.querySelector('.sortear')
+
+//SENDO FEITO:
+//sortear carta
+
+buscarPersonagens()
+
+async function buscarPersonagens() {
+    const res = await fetch(personagensAPI)
+    personagens = await res.json()
+    
+    
+    sortear.addEventListener('click', () => {
+        return parseInt(Math.random() * personagens.length)
+    }) 
+
+    console.log()
+
+};
+//conserta isso ai pelo amor de deus
+
+
+let maquina = {
+    nome: "Máquina",
+    vitorias: 0,
+    empates: 0,
+    derrotas: 0,
+    pontos: 0
+};
+
+
+
+
+
+
+
+
+
+
+
+//A FAZER:
+//exibir opções
+//obter atributo
+//jogar
+//exibir carta jogador
+//exibir carta maquina
+
+//PRONTOS:
+//adicionar jogador FEITO
+
+//jogar novamente FEITO
+ const jogarNovamente = document.querySelector('#btn-jogar-novamente')
+ jogarNovamente.addEventListener('click', () => {
+     window.location.reload()
+ })
